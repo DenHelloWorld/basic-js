ecce0c4f84490231c31031ff2a20c0fb009dbecc
= <=>
2023-04-17 19:13:06 +0300

    feat: end dream and encode line exam (Mon, Apr 17, 2023 07:13:05 PM)

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -1,16 +1,4 @@
 
-
-/**
- * Given some integer, find the maximal number you can obtain
- * by deleting exactly one digit of the given number.
- *
- * @param {Number} n
- * @return {Number}
- *
- * @example
- * For n = 152, the output should be 52
- *
- */
 function deleteDigit(n) {
    const numberStr = n.toString();
    const numbersArray = [];
